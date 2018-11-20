f73e17e530500141caa7035cc651324a239b2bb4
OleksiiH <haivorolexiy@gmail.com>
2018-11-20 20:24:58 +0200

    detail

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import AppolliClient, { ApolloClient } from 'apollo-client';
 import { AppolloProvider, ApolloProvider } from 'react-apollo';
 import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail'
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import App from './components/App';
-import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({});
 
@@ -16,6 +17,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList}></IndexRoute>
           <Route path="songs/new" component={SongCreate}></Route>
+          <Route path="songs/:id" component={SongDetail}></Route>
         </Route>
       </Router>
     </ApolloProvider>
